a4efd0e758f8e7c9b37fa1a1841289d9ebe96cdd
punita-1 <punitagaba1@gmail.com>
2024-03-09 16:10:25 +0530

    gallery responsive

--- src/Myswiper.jsx
+++ src/Myswiper.jsx
@@ -6,119 +6,252 @@ import 'swiper/css/pagination';
 import './Swiperstyle.css';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 
-// Swiper component for each stack
-const MySwiper = ({ images, openModal }) => {
-  return (
-    <div className="swiper-container">
-      <Swiper
-        effect={'coverflow'}
-        loop={true}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={image}
-              alt={`Image ${index}`}
-              onClick={() => openModal(image)} // Add onClick handler
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
-};
-
 export default function App() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState('');
-
-  const openModal = (image) => {
-    setSelectedImage(image);
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
-  // Define images for each Swiper
-  const images1 = [
-    'public/images/technova_gallery/logos/technova_rope.jpg',
-    'public/images/technova_gallery/Vc_starting/starting.jpg',
-    'public/images/technova_gallery/Vc_starting/vc_project_chkecking.jpg',
-    'public/images/technova_gallery/Vc_starting/starting.jpg',
-    // Add more images as needed
-  ];
-
-  const images2 = [
-    'public/images/technova_gallery/logos/thermocol_techno.jpg',
-    'public/images/technova_gallery/welcomes/flower_welcome.jpg',
-    'public/images/technova_gallery/welcomes/welcome_2.jpg',
-    'public/images/technova_gallery/welcomes/welcome_thanks.jpg',
-    'public/images/technova_gallery/welcomes/welcome_type.jpg',
-    'public/images/technova_gallery/welcomes/welcome5.jpg',
-    'public/images/technova_gallery/welcomes/welcome6.jpg',
-    // Add more images as needed
-  ];
-
-  const images3 = [
-    'public/images/technova_gallery/events/cards.jpg',
-    'public/images/technova_gallery/events/hand_sensor.jpg',
-    'public/images/technova_gallery/events/presentation_cse.jpg',
-    'public/images/technova_gallery/events/Random.jpg',
-    'public/images/technova_gallery/events/robo_soccer.jpg',
-    'public/images/technova_gallery/events/robo_soccer2.jpg',
-    'public/images/technova_gallery/events/thinkbots1.jpg',
-    'public/images/technova_gallery/events/thinkbots2.jpg',
-    // Add more images as needed
-  ];
-
-  const images4 = [
-    'public/images/technova_gallery/prize/price2.jpg',
-    'public/images/technova_gallery/prize/Prize1.jpg',
-    'public/images/technova_gallery/prize/prize3.jpg',
-    'public/images/technova_gallery/prize/prize4.jpg',
-    // Add more images as needed
-  ];
-  const images5 = [
-    'public/images/technova_gallery/group_pics/gp_pic3.jpg',
-    'public/images/technova_gallery/group_pics/gp_pic4.jpg',
-    'public/images/technova_gallery/group_pics/group_photo.jpg',
-    'public/images/technova_gallery/group_pics/group_pic2.jpg',
-    // Add more images as needed
-  ];
-
   return (
     <>
-      {/* Stack Swiper components */}
-      <MySwiper images={images1} openModal={openModal} />
-      <MySwiper images={images2} openModal={openModal} />
-      <MySwiper images={images3} openModal={openModal} />
-      <MySwiper images={images4} openModal={openModal} />
-      <MySwiper images={images5} openModal={openModal} />
+      <div className="swiper-container">
+        <Swiper
+          effect={'coverflow'}
+          // loop={true} // Enable looping
+          grabCursor={true}
+          // loop={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            // loop: true,
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/logos/technova_rope.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/Vc_starting/starting.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/Vc_starting/vc_project_chkecking.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/Vc_starting/vc_pic.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/audi_mv_night.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/auditorium_night.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/random_2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/random.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="swiper-container">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          // loop={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/logos/thermocol_techno.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/welcomes/flower_welcome.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/welcomes/welcome_2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/welcomes/welcome_thanks.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/welcomes/welcome_type.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/welcomes/welcome5.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/welcomes/welcome6.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/wide_view_cool.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="swiper-container">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          // loop={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/cards.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/hand_sensor.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/presentation_cse.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/Random.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/robo_soccer.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/robo_soccer2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/thinkbots1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/events/thinkbots2.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="swiper-container">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          // loop={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/prize/price2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/prize/Prize1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/prize/prize3.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/prize/prize4.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/day_cloud_image_library_wide_view.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/IMG_20240207_143133 (1).jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/IMG20240207140632.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/library_clouds_image.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="swiper-container">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          // loop={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/group_pics/gp_pic3.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/group_pics/group_photo.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/group_pics/gp_pic4.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/technova_gallery/group_pics/group_pic2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/mv_block_2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/mv_block.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/dcrust_images/night_Saraswati_library.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="public/images/events_profile_pic/Decoration.jpg" />
+          </SwiperSlide>
 
-      {/* Modal */}
-      {modalOpen && (
-        <div className="modal" onClick={closeModal}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <span className="close" onClick={closeModal}>&times;</span>
-            <img src={selectedImage} alt="Selected" className="modal-image" />
-          </div>
-        </div>
-      )}
+        </Swiper>
+      </div>
     </>
   );
 }
