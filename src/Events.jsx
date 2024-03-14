1465b916cfcd415ddadd5506505d534c8d4e566c
punita-1 <punitagaba1@gmail.com>
2024-03-14 14:51:24 +0530

    tabs added on events page

--- src/Events.jsx
+++ src/Events.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import CardGroup from './Cardgroup';
 import events from "./assets/data"
 import SearchBar from './SearchBar';
+import { Tabs, Tab } from 'react-bootstrap';
 
 const Event = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -14,7 +15,7 @@ const Event = () => {
     if (!query) {
       setSearchResults(events);
     } else {
-      const results = searchResults.filter((event)=> event.category.toLowerCase().includes(query.toLowerCase()) || event.title.toLowerCase().includes(query.toLowerCase()));
+      const results = searchResults.filter((event) => event.category.toLowerCase().includes(query.toLowerCase()) || event.title.toLowerCase().includes(query.toLowerCase()));
       console.log(results);
       setSearchResults(results);
     }
@@ -39,114 +40,122 @@ const Event = () => {
       <br />
       <br />
 
-      <h2><b>CENTRALIZED EVENTS</b></h2>
-      <CardGroup
-        heading="Unite & Ignite: Events for All Students"
-        events={searchResults.filter((ev)=>ev.category==="centralized")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <h2><b>DEPARTMENT EVENTS</b></h2>
-      <CardGroup
-        heading="Biotechnology department #ENGENISIS"
-        events={searchResults.filter((ev)=>ev.category==="biotechnology")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department of Management Studies(MBA) #ZENITH "
-        events={searchResults.filter((ev)=>ev.category==="management")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="DEPARTMENT OF AGRICULTURE ENGINEERING #CEEES "
-        events={searchResults.filter((ev)=>ev.category==="agriculture")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="DEPARTMENT OF BIO MEDICAL ENGINEERING(BME) #MEDITRONICA "
-        events={searchResults.filter((ev)=>ev.category==="biomedical")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department of Electrical Engineering"
-        events={searchResults.filter((ev)=>ev.category==="electrical")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department of Chemical Engineering #MANTHAN"
-        events={searchResults.filter((ev)=>ev.category==="chemical")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="DEPARTMENT OF ELECTRONICS AND COMMUNICATION ENGINEERING #INTEC"
-        events={searchResults.filter((ev)=>ev.category==="electronics")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="DEPARTMENT OF Mathematics #MATHEMAGICIANS"
-        events={searchResults.filter((ev)=>ev.category==="mathematics")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="DEPARTMENT OF PHYSICS #RAMAN"
-        events={searchResults.filter((ev)=>ev.category==="physics")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department of COMPUTER SCIENCE ENGINEERING(CSE) #COSMOS"
-        events={searchResults.filter((ev)=>ev.category==="computer")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department OF MECHANICAL ENGINEERING(MED) #SOMEC"
-        events={searchResults.filter((ev)=>ev.category==="Mechanical")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department OF CHEMISTRY #RASAYANAM"
-        events={searchResults.filter((ev)=>ev.category==="chemistry")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Department OF ARCHITECHTURE"
-        events={searchResults.filter((ev)=>ev.category==="architecture")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="CIVIL DEPARTMENT #NIRMAN"
-        events={searchResults.filter((ev)=>ev.category==="civil")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <h2>SOCIETY EVENTS</h2>
-      <CardGroup
-        heading="SUNSHINE"
-        events={searchResults.filter((ev)=>ev.category==="sunshine")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="E-Cell"
-        events={searchResults.filter((ev)=>ev.category==="e-cell")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="LISOC (LITERARY SOCIETY) "
-        events={searchResults.filter((ev)=>ev.category==="lisoc")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="SAVERA"
-        events={searchResults.filter((ev)=>ev.category==="savera")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="Thinkbots"
-        events={searchResults.filter((ev)=>ev.category==="Thinkbots")}
-        handleRegisterClick={handleRegisterClick}
-      />
-      <CardGroup
-        heading="OPEN SOURCE DEVELOPERS COMMUNITY #DCRUSTODC"
-        events={searchResults.filter((ev)=>ev.category==="dcrustodc")}
-        handleRegisterClick={handleRegisterClick}
-      />
+      <Tabs>
+        <Tab eventKey="centralized" title="Centralized">
+          <h2><b>CENTRALIZED EVENTS</b></h2>
+          <CardGroup
+            heading="Unite & Ignite: Events for All Students"
+            events={searchResults.filter((ev) => ev.category === "centralized")}
+            handleRegisterClick={handleRegisterClick}
+          />
+        </Tab>
+        <Tab eventKey="dep" title="Department">
+          <h2><b>DEPARTMENT EVENTS</b></h2>
+          <CardGroup
+            heading="Biotechnology department #ENGENISIS"
+            events={searchResults.filter((ev) => ev.category === "biotechnology")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department of Management Studies(MBA) #ZENITH "
+            events={searchResults.filter((ev) => ev.category === "management")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="DEPARTMENT OF AGRICULTURE ENGINEERING #CEEES "
+            events={searchResults.filter((ev) => ev.category === "agriculture")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="DEPARTMENT OF BIO MEDICAL ENGINEERING(BME) #MEDITRONICA "
+            events={searchResults.filter((ev) => ev.category === "biomedical")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department of Electrical Engineering"
+            events={searchResults.filter((ev) => ev.category === "electrical")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department of Chemical Engineering #MANTHAN"
+            events={searchResults.filter((ev) => ev.category === "chemical")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="DEPARTMENT OF ELECTRONICS AND COMMUNICATION ENGINEERING #INTEC"
+            events={searchResults.filter((ev) => ev.category === "electronics")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="DEPARTMENT OF Mathematics #MATHEMAGICIANS"
+            events={searchResults.filter((ev) => ev.category === "mathematics")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="DEPARTMENT OF PHYSICS #RAMAN"
+            events={searchResults.filter((ev) => ev.category === "physics")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department of COMPUTER SCIENCE ENGINEERING(CSE) #COSMOS"
+            events={searchResults.filter((ev) => ev.category === "computer")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department OF MECHANICAL ENGINEERING(MED) #SOMEC"
+            events={searchResults.filter((ev) => ev.category === "Mechanical")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department OF CHEMISTRY #RASAYANAM"
+            events={searchResults.filter((ev) => ev.category === "chemistry")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Department OF ARCHITECHTURE"
+            events={searchResults.filter((ev) => ev.category === "architecture")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="CIVIL DEPARTMENT #NIRMAN"
+            events={searchResults.filter((ev) => ev.category === "civil")}
+            handleRegisterClick={handleRegisterClick}
+          />
+        </Tab>
+        <Tab eventKey="Society" title="Society">
+          <h2>SOCIETY EVENTS</h2>
+          <CardGroup
+            heading="SUNSHINE"
+            events={searchResults.filter((ev) => ev.category === "sunshine")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="E-Cell"
+            events={searchResults.filter((ev) => ev.category === "e-cell")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="LISOC (LITERARY SOCIETY) "
+            events={searchResults.filter((ev) => ev.category === "lisoc")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="SAVERA"
+            events={searchResults.filter((ev) => ev.category === "savera")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="Thinkbots"
+            events={searchResults.filter((ev) => ev.category === "Thinkbots")}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <CardGroup
+            heading="OPEN SOURCE DEVELOPERS COMMUNITY #DCRUSTODC"
+            events={searchResults.filter((ev) => ev.category === "dcrustodc")}
+            handleRegisterClick={handleRegisterClick}
+          />
+        </Tab>
+      </Tabs>
 
       {/* // Add more CardGroup components as needed */}
       {showPopup && (
